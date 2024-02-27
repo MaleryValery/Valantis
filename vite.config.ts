a0000378444edc560c243a81c56e1023eaee4cf9
Valeriia Melnikov <valeriia.melnikov@gmail.com>
2024-02-27 14:43:14 +0200

    feat: update config for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     open: true,
   },
-  plugins: [react()], 
-
+  plugins: [react()],
+  base: '/valantis-vm/',
 });
